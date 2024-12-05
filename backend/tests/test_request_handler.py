a9634d4f0f24826b4a6b3dc9ced50ec205bc3415
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-12-05 15:14:00 +0100

    Added tests for request handler

--- backend/tests/test_request_handler.py
+++ backend/tests/test_request_handler.py
@@ -6,7 +6,37 @@ from llmany_backend.model_handler_factory import ModelHandlerFactory
 from llmany_backend.requests import AllChatRequest, DeleteChatRequest, ChatHistoryRequest, MessageRequest, NewChatRequest
 
 
-@pytest.mark.parametrize("request, expected_dict", (""))
+@pytest.mark.parametrize("request, expected_dict", [
+    ('{'
+    '"type": "new_chat",'    
+    '"model_type": the type/provider of the model,'   
+    '"model": the name of the model}',
+    {   
+    "type": "new_chat",   
+    "model_type": "ChatGPT",   
+    "model": "GPT-4o"    
+    }),
+    ('{"type": "delete_chat", "chat_id": "1234",}',
+    {    
+    "type": "delete_chat",   
+    "chat_id": "1234",      
+    }),
+    ('{"type": "all_chats"}',
+    {    
+    "type": "all_chats",   
+    }),
+    ('{"type": "chat_history", "chat_id": "1234"}',
+    {   
+    "type": "chat_history",   
+    "chat_id": "1234"   
+    }),
+    ('{"type": "message", "chat_id": "1234", "contents": "Hello world!"}',
+    {
+    "type": "message",   
+    "chat_id": "1234",   
+    "contents": "Hello world!"
+    }),
+])
 def test_parse(request, expected_dict, monkeypatch):
     monkeypatch.setattr('bultins.input', lambda _ : request)
     handler = RequestHandler(database_handler_factory=DatabaseHandlerFactory(), 
