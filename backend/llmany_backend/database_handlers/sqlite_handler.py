e2f0075899436f2c63d56f17769de0b24838a3d4
Oskar Kuliński <109166833+oskarkulinski@users.noreply.github.com>
2024-12-12 13:00:10 +0100

    Added first version of SQLiteHandler (#57)
    * Added first version of SQLiteHandler
    
    Added add_message_to_chat, get_chat_history, create_new_chat methods, and made some fixes to the tests for these classes
    
    * Added a DatabaseHandlerFactory with support for SQLite3

--- backend/llmany_backend/database_handlers/sqlite_handler.py
+++ backend/llmany_backend/database_handlers/sqlite_handler.py
@@ -1,26 +1,80 @@
 from typing import Tuple, Dict
+import sqlite3
 
 from llmany_backend.database_handler import DatabaseHandler
 
 
 class SQLiteHandler(DatabaseHandler):
-    def __init__(self, connection=None) -> None:
-        self.connection = connection
+    def __init__(self, connection: sqlite3.Connection) -> None:
+        self.connection: sqlite3.Connection = connection
 
     def get_model_for_chat(self, id: str) -> Tuple[str, str]:
-        return ...
+        raise NotImplementedError
 
-    def create_new_chat(self) -> str:
-        return ...
+    def create_new_chat(self, model_type: str, model: str) -> int:
+        cursor = self.connection.cursor()
 
-    def add_message_to_chat(self, id: str, message: str) -> None:
-        pass
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS chats (chat_id INTEGER PRIMARY KEY, name TEXT, model_type TEXT, model TEXT)"
+        )
+        self.connection.commit()
 
-    def get_chat_history(self, id: str) -> Dict:
-        return ...
+        cursor.execute("SELECT MAX(chat_id) AS max_id FROM chats")
+
+        result = cursor.fetchone()
+
+        chat_id: int = result[0] + 1 if result[0] is not None else 0
+
+        cursor.execute(
+            "INSERT INTO chats (chat_id, name, model_type, model) VALUES (?, ?, ?, ?)",
+            (chat_id, "Test Chat", model_type, model),
+        )
+
+        self.connection.commit()
+        cursor.close()
+        return chat_id
+
+    def add_message_to_chat(self, chat_id: int, role: str, message: str) -> None:
+        cursor = self.connection.cursor()
+
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS messages (chat_id INT NOT NULL, message_id INT NOT NULL, role TEXT,message TEXT,PRIMARY KEY (chat_id, message_id));"
+        )
+        self.connection.commit()
+
+        cursor.execute(
+            "SELECT MAX(message_id) AS max_id FROM messages WHERE chat_id = ?",
+            (chat_id,),
+        )
+
+        result = cursor.fetchone()
+
+        message_id = result[0] + 1 if result[0] is not None else 0
+
+        cursor.execute(
+            "INSERT INTO messages (chat_id, message_id, role, message) VALUES (?, ?, ?, ?)",
+            (chat_id, message_id, role, message),
+        )
+
+        self.connection.commit()
+        cursor.close()
+
+    def get_chat_history(self, chat_id: int) -> list[tuple]:
+        cursor = self.connection.cursor()
+
+        cursor.execute(
+            "SELECT role, message FROM messages WHERE chat_id = ? ORDER BY message_id",
+            (chat_id,),
+        )
+
+        result = cursor.fetchall()
+
+        cursor.close()
+
+        return result
 
     def get_all_chats(self) -> Dict:
-        return ...
+        raise NotImplementedError
 
     def remove_chat(self, id: str) -> None:
-        pass
+        raise NotImplementedError
