38cd0ed5d51b764630243459a33e199e2153434a
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-12-12 13:24:31 +0100

    Implemented parse method in request-handler

--- backend/llmany_backend/request_handler.py
+++ backend/llmany_backend/request_handler.py
@@ -1,3 +1,5 @@
+import json
+
 from llmany_backend.database_handler_factory import DatabaseHandlerFactory
 from llmany_backend.model_handler_factory import ModelHandlerFactory
 from llmany_backend.request import Request
@@ -6,14 +8,15 @@ from llmany_backend.request import Request
 class RequestHandler:
     def __init__(
         self,
-        database_handler_factory: "DatabaseHandlerFactory",
-        model_handler_factory: "ModelHandlerFactory",
+        database_handler_factory: DatabaseHandlerFactory,
+        model_handler_factory: ModelHandlerFactory,
     ) -> None:
         self.database_handler_factory: DatabaseHandlerFactory = database_handler_factory
         self.model_handler_factory: ModelHandlerFactory = model_handler_factory
 
     def parse(self) -> dict:
-        return ...
-    
-    def create_request(self, request_data :dict) -> Request:
-        return ...
+        request_json = input()
+        return json.loads(request_json)
+
+    def create_request(self, request_data: dict) -> Request:
+        raise NotImplementedError("create_request method must be implemented")
