2429915849b8153d94c166da24e4a50cbdeb771d
Oskar Kuliński <109166833+oskarkulinski@users.noreply.github.com>
2024-12-12 12:56:32 +0100

    Added a working version of OpenAIHandler, with basic message sending (#62)
    * Added a working version of OpenAIHandler, with basic message sending
    
    * Fixed mocking in OpenAIHandler test
    
    * Fixed wrong mypy script being called
    
    * Changed permissions in run-mypy.sh
    
    * Added installing poetry to pre-commit workflow
    
    * Added installing poetry to pre-commit workflow
    
    * Fixed poetry being run from wrong directory

--- backend/llmany_backend/model_handlers/openai_handler.py
+++ backend/llmany_backend/model_handlers/openai_handler.py
@@ -1,9 +1,18 @@
+from openai import OpenAI
+
 from llmany_backend.model_handler import ModelHandler
 
-
 class OpenAIHandler(ModelHandler):
-    def send_message(self, message: str) -> None:
-        pass
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.client = OpenAI(api_key=api_key)
+
+    def send_message(self, model: str, messages: list[dict[str:str]]) -> None:
+        completion = self.client.chat.completions.create(
+            model=model,
+            messages=messages
+        )
+        return completion.choices[0].message.content
 
     def parse_message(self) -> str:
         return ...
--- backend/tests/tests_model_handlers/test_openai_handler.py
+++ backend/tests/tests_model_handlers/test_openai_handler.py
@@ -1,19 +1,27 @@
+from unittest.mock import MagicMock
+
 from llmany_backend.model_handlers.openai_handler import OpenAIHandler
 
 
 def test_send_message(mocker):
-    mock_create = mocker.patch("client.chat.completions.create")
-    mock_create.return_value = {
-        "choices": [{"message": {"content": "Hello, this is Skynet!"}}]
-    }
+    mock_openai_instance = MagicMock()
+    mock_choice = MagicMock()
+    mock_choice.message.content = "Hello, this is Skynet!"
+    mock_openai_instance.chat.completions.create.return_value = MagicMock(
+        choices=[mock_choice]
+    )
+    mocker.patch("llmany_backend.model_handlers.openai_handler.OpenAI", return_value=mock_openai_instance)
 
-    handler = OpenAIHandler()
+    handler = OpenAIHandler(api_key= " ")
 
-    response = handler.send_message("Hello")
+    response = handler.send_message(
+        model="gpt-4",
+        messages=[{"role": "user", "content": "Hello"}]
+    )
 
     assert response == "Hello, this is Skynet!"
 
-    mock_create.assert_called_once_with(
+    mock_openai_instance.chat.completions.create.assert_called_once_with(
         model="gpt-4",
         messages=[{"role": "user", "content": "Hello"}],
     )
