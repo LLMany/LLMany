bf8e617f248981fa53cbd5a32ed5b6f2aec41286
KajetanOzog <kajetanozog@gmail.com>
2024-12-13 21:38:37 +0100

    Implemented delete chat request class

--- backend/tests/tests_requests/test_delete_chat_request.py
+++ backend/tests/tests_requests/test_delete_chat_request.py
@@ -1,11 +1,12 @@
+import json
 from unittest.mock import MagicMock
 from llmany_backend.database_handler import DatabaseHandler
 from llmany_backend.requests.delete_chat_request import DeleteChatRequest
 
 
 def test_delete_chat_request_initialization():
-    mock_database_handler = MagicMock(DatabaseHandler)
-    chat_id = "12345"
+    mock_database_handler = MagicMock(spec=DatabaseHandler)
+    chat_id = 12345
 
     request = DeleteChatRequest(mock_database_handler, chat_id)
 
@@ -13,16 +14,27 @@ def test_delete_chat_request_initialization():
     assert request.chat_ID == chat_id
 
 
-def test_execute_delete_chat_request(mocker):
-    mock_database_handler = MagicMock(DatabaseHandler)
+def test_delete_chat_request_from_dict():
+    mock_database_handler = MagicMock(spec=DatabaseHandler)
+    request_dict = {"chat_id": 12345}
 
-    mock_database_handler.delete_chat.return_value = "Chat deleted successfully"
+    request = DeleteChatRequest.from_dict(request_dict, mock_database_handler)
 
-    chat_id = "12345"
+    assert request.database_handler == mock_database_handler
+    assert request.chat_ID == 12345
+
+
+def test_execute_delete_chat_request(mocker, capsys):
+    mock_database_handler = mocker.Mock(spec=DatabaseHandler)
+
+    chat_id = 12345
+    mock_database_handler.remove_chat.return_value = True
+
     request = DeleteChatRequest(mock_database_handler, chat_id)
+    request.execute()
 
-    result = request.execute()
+    captured = capsys.readouterr()
+    expected_output = {"type": "delete_chat", "chat_id": chat_id, "status": True}
 
-    assert result == "Chat deleted successfully"
-
-    mock_database_handler.delete_chat.assert_called_once_with(chat_id)
+    assert json.loads(captured.out) == expected_output
+    mock_database_handler.remove_chat.assert_called_once_with(chat_id)
