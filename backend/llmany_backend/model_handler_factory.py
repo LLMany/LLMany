64bbed6da9472e3256e1b66f51beafc188581b75
Oskar Kuliński <oskarkulinski3@gmail.com>
2025-01-02 16:44:09 +0100

    Fixed model name case

--- backend/llmany_backend/model_handler_factory.py
+++ backend/llmany_backend/model_handler_factory.py
@@ -1,14 +1,15 @@
 from llmany_backend.model_handler import ModelHandler
-from llmany_backend.model_handlers import GoogleHandler, OpenAIHandler, AnthropicHandler 
+from llmany_backend.model_handlers import GoogleHandler, OpenAIHandler, AnthropicHandler
+
 
 class ModelHandlerFactory:
     def create_model_handler(self, model_type: str, api_key: str) -> ModelHandler:
         match model_type:
-            case "google":
-                return GoogleHandler(api_key)
-            case "openai":
+            case "OpenAI":
                 return OpenAIHandler(api_key)
-            case "anthropic":
+            case "Google":
+                return GoogleHandler(api_key)
+            case "Anthropic":
                 return AnthropicHandler(api_key)
             case _:
                 raise ValueError(f"Unsupported model type: {model_type}")
