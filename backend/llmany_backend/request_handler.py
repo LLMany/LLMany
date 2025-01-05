c47dc14f686d90040430570d0a2bd33076074972
Oskar Kuliński <oskarkulinski3@gmail.com>
2025-01-05 14:18:57 +0100

    Added api key requests to RequestHandler, refactored RequestHandler tests and added new ones

--- backend/llmany_backend/request_handler.py
+++ backend/llmany_backend/request_handler.py
@@ -10,6 +10,9 @@ from llmany_backend.requests import (
     DeleteChatRequest,
     NewChatRequest,
     MessageRequest,
+    AddApiKeyRequest,
+    RemoveApiKeyRequest,
+    CheckApiKeyRequest,
 )
 
 
@@ -23,48 +26,36 @@ class RequestHandler:
         self.database_handler_factory: DatabaseHandlerFactory = database_handler_factory
         self.model_handler_factory: ModelHandlerFactory = model_handler_factory
         self.connection = connection
+        self.database_handler = self.database_handler_factory.create_database_handler(
+            "SQLite3", self.connection
+        )
 
     def parse(self, request_json: str) -> dict:
         return json.loads(request_json)
 
     def create_request(self, request_data: dict) -> Request:
-        database = "SQLite3"
         match request_data["type"]:
             case "all_chats":
-                return AllChatsRequest.from_dict(
-                    request_data,
-                    self.database_handler_factory.create_database_handler(
-                        database, self.connection
-                    ),
-                )
+                return AllChatsRequest.from_dict(request_data, self.database_handler)
             case "chat_history":
-                return ChatHistoryRequest.from_dict(
-                    request_data,
-                    self.database_handler_factory.create_database_handler(
-                        database, self.connection
-                    ),
-                )
+                return ChatHistoryRequest.from_dict(request_data, self.database_handler)
             case "delete_chat":
-                return DeleteChatRequest.from_dict(
-                    request_data,
-                    self.database_handler_factory.create_database_handler(
-                        database, self.connection
-                    ),
-                )
+                return DeleteChatRequest.from_dict(request_data, self.database_handler)
             case "new_chat":
-                return NewChatRequest.from_dict(
-                    request_data,
-                    self.database_handler_factory.create_database_handler(
-                        database, self.connection
-                    ),
-                )
+                return NewChatRequest.from_dict(request_data, self.database_handler)
             case "message":
                 return MessageRequest.from_dict(
                     request_data,
-                    self.database_handler_factory.create_database_handler(
-                        database, self.connection
-                    ),
+                    self.database_handler,
                     self.model_handler_factory,
                 )
+            case "add_api_key":
+                return AddApiKeyRequest.from_dict(request_data, self.database_handler)
+            case "remove_api_key":
+                return RemoveApiKeyRequest.from_dict(
+                    request_data, self.database_handler
+                )
+            case "check_api_key":
+                return CheckApiKeyRequest.from_dict(request_data, self.database_handler)
             case _:
                 raise ValueError("Invalid request type")
