ed3009ea82b9793e0933eba0a44ae386adb0a995
Kajetan <110201282+KajetanOzog@users.noreply.github.com>
2025-01-09 13:16:07 +0100

    Added CheckApiKey class and tests (#99)
    Co-authored-by: Oskar Kulinski <oskarkulinski3@gmail.com>

--- backend/llmany_backend/llmany_requests/check_api_key_request.py
+++ backend/llmany_backend/llmany_requests/check_api_key_request.py
@@ -1,16 +1,27 @@
+import json
 from llmany_backend.database_handler import DatabaseHandler
 from llmany_backend.request import Request
 
 
 class CheckApiKeyRequest(Request):
-    def __init__(
-        self, database_handler: DatabaseHandler, model_type: str, api_key: str
-    ) -> None:
-        raise NotImplementedError
+    def __init__(self, database_handler: DatabaseHandler, model_type: str) -> None:
+        self.database_handler: DatabaseHandler = database_handler
+        self.model_type: str = model_type
 
     @classmethod
     def from_dict(cls, request: dict, database_handler: DatabaseHandler):
-        raise NotImplementedError
+        return cls(database_handler, request["model_type"])
 
     def execute(self) -> None:
-        raise NotImplementedError
+        exists: bool
+        if self.database_handler.get_api_key(self.model_type) is None:
+            exists = False
+        else:
+            exists = True
+
+        returned_value = {
+            "type": "check_api_key",
+            "chat_id": self.model_type,
+            "exists": exists,
+        }
+        print(json.dumps(returned_value))
