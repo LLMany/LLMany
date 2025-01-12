9f2ce8ffb0e0c8311e1f0b4a515ad016aed943a6
Oskar Kulinski <oskarkulinski3@gmail.com>
2025-01-12 23:23:42 +0100

    Added first version of anthropic handler

--- backend/llmany_backend/model_handlers/anthropic_handler.py
+++ backend/llmany_backend/model_handlers/anthropic_handler.py
@@ -1,14 +1,26 @@
+from anthropic import Anthropic
+
 from llmany_backend.model_handler import ModelHandler
 
 
 class AnthropicHandler(ModelHandler):
     def __init__(self, api_key: str):
         self.api_key = api_key
+        self.client = Anthropic(api_key=api_key)
 
     def send_message(
         self, model: str, message: str, history: list[dict[str, str]]
     ) -> str:
-        raise NotImplementedError
+        history.append({"role": "user", "content": message})
+        completion = self.client.messages.create(  # type: ignore
+            model=model,  # type: ignore
+            messages=history,  # type: ignore
+        )
+        return (
+            completion.content
+            if completion.content is not None
+            else "An unexpected error occurred, please try sending the message again"
+        )
 
     def parse_message(self) -> str:
         raise NotImplementedError
