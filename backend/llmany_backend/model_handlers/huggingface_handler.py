c57ebc8dd0051ae881d236e9c4a2be8bcc81515f
Oskar Kulinski <oskarkulinski3@gmail.com>
2025-01-23 13:41:29 +0100

    Revert "Added proper handling of Huggingface api keys"
    This reverts commit 572cd88eb7224e3881646ef35909df12722a090d.

--- backend/llmany_backend/model_handlers/huggingface_handler.py
+++ backend/llmany_backend/model_handlers/huggingface_handler.py
@@ -4,17 +4,15 @@ from llmany_backend.model_handler import ModelHandler
 
 
 class HuggingfaceHandler(ModelHandler):
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self):
+        pass
 
     def send_message(
         self, model: str, message: str, history: list[dict[str, str]]
     ) -> str:
         history.append({"role": "user", "content": message})
 
-        assistant = pipeline(
-            "text-generation", model=model, device_map="auto", token=self.api_key
-        )
+        assistant = pipeline("text-generation", model=model, device_map="auto")
 
         output = assistant(history, max_new_tokens=512)
         return output[0]["generated_text"][-1]["content"]
